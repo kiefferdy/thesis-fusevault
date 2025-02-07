0af4e59022a41be71b9353e5c610028ba3b83d67
KnightofSpace <jeanine_marie_ignacio@dlsu.edu.ph>
2025-02-07 13:46:56 +0800

    Update route_conn_db_ipfs.py

--- app/routes/route_conn_db_ipfs.py
+++ app/routes/route_conn_db_ipfs.py
@@ -10,3 +10,10 @@ async def receive_doc_db(document_id: str):
     Testing parameter passing between Connection Router and MongoDB Router.
     """
     return await db.get_document(document_id)
+
+@router.post("/test2/{cid}")
+async def receive_url_ipfs(cid: str):
+    """
+    Testing parameter passing between Connection Router and IPFS Router.
+    """
+    return await ipfs.get_file_contents(cid)
