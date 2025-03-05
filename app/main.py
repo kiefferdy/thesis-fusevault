4390176e7fdc0d8067e91f4dcbc456914c85a5ba
kiefferdy <kieffer_recatody@dlsu.edu.ph>
2025-03-06 02:45:08 +0800

    Reworked route_storage and replaced it with route_upload

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # ✅ Import CORS middleware
+from fastapi.middleware.cors import CORSMiddleware
 
-# ✅ Import your routers
 from app.core.route_sc import router as sc_router
 from app.core.route_db import router as db_router
 from app.core.route_ipfs import router as ipfs_router
 from app.utilities.route_verify import router as verify_router
-from app.connectors.route_storage import router as storage_router
+from app.connectors.route_upload import router as upload_router
 from app.external.route_auth import router as auth_router
 from app.external.route_transactions import router as transactions_router
 from app.external.route_sessions import router as session_router
@@ -27,7 +26,7 @@ routers = [
     db_router,
     ipfs_router,
     verify_router,
-    storage_router,
+    upload_router,
     auth_router,
     transactions_router,
     session_router,
@@ -36,4 +35,3 @@ routers = [
 
 for router in routers:
     app.include_router(router)
-
